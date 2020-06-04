d485e0d76f0aeffd9cb03fa94dc24c0a35fa108c
Macarena Saez <makadaira@gmail.com>
2020-06-04 17:39:45 -0400

    primer commit

---
+++ src/index.js
@@ -0,0 +1,184 @@
+
+//Variables
+const container = document.querySelector('.contenido');
+const question1 = document.getElementById('question1');
+
+//listener
+question1.addEventListener('click', () => {
+    const row = document.createElement('div')
+    row.innerHTML = `
+    <section>
+        <h1 class="title">${question1.textContent}</h1>
+        <div class="divider"></div>
+        </section>
+        <p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Curabitur ac mi volutpat, feugiat est eget, bibendum nulla.
+        Curabitur ut velit id odio ullamcorper porttitor consectetur at sem.
+        Cras tempor odio ac eros suscipit scelerisque.
+        Aenean rutrum lacus ut sem dignissim, at fringilla diam sollicitudin.
+        Donec id risus aliquam, ultrices diam et, mattis ipsum.
+        Nulla venenatis lectus non velit molestie faucibus.
+        Proin fringilla ex in sapien tincidunt, a interdum elit tempor.</p>
+    `
+    container.innerHTML= "";
+    container.appendChild(row);
+})
+
+//VARIABLES
+let cont = document.querySelector('.contenido');
+let question2 = document.getElementById('question2');
+
+
+question2.addEventListener('click',() =>{
+    const row = document.createElement('div')
+    row.innerHTML = `
+    <section> 
+    <h1 class="title">${question2.textContent}</h1>
+    <div class="divider"></div>
+    </section>
+    <p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Curabitur ac mi volutpat, feugiat est eget, bibendum nulla.
+    Curabitur ut velit id odio ullamcorper porttitor consectetur at sem.
+    Cras tempor odio ac eros suscipit scelerisque.
+    Aenean rutrum lacus ut sem dignissim, at fringilla diam sollicitudin.
+    Donec id risus aliquam, ultrices diam et, mattis ipsum.
+    Nulla venenatis lectus non velit molestie faucibus.
+    Proin fringilla ex in sapien tincidunt, a interdum elit tempor</p>
+    `
+
+container.innerHTML= "";
+    container.appendChild(row)
+})
+
+//VARIABLES
+let conta = document.querySelector('.contenido');
+let question3 = document.getElementById('question3');
+
+
+question3.addEventListener('click',() =>{
+    const row = document.createElement('div')
+    row.innerHTML = `
+    <section> 
+    <h1 class="title">${question3.textContent}</h1>
+    <div class="divider"></div>
+    </section>
+    <p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Curabitur ac mi volutpat, feugiat est eget, bibendum nulla.
+    Curabitur ut velit id odio ullamcorper porttitor consectetur at sem.
+    Cras tempor odio ac eros suscipit scelerisque.
+    Aenean rutrum lacus ut sem dignissim, at fringilla diam sollicitudin.
+    Donec id risus aliquam, ultrices diam et, mattis ipsum.
+    Nulla venenatis lectus non velit molestie faucibus.
+    Proin fringilla ex in sapien tincidunt, a interdum elit tempor</p>
+    `
+
+    container.innerHTML= "";
+    container.appendChild(row)
+})
+
+
+//VARIABLES
+let contai = document.querySelector('.contenido');
+let question4 = document.getElementById('question4');
+
+//LIsTENER
+question4.addEventListener('click',() =>{
+    const row = document.createElement('div')
+    row.innerHTML = `
+    <section> 
+    <h1 class="title">${question4.textContent}</h1>
+    <div class="divider"></div>
+    </section>
+    <p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Curabitur ac mi volutpat, feugiat est eget, bibendum nulla.
+    Curabitur ut velit id odio ullamcorper porttitor consectetur at sem.
+    Cras tempor odio ac eros suscipit scelerisque.
+    Aenean rutrum lacus ut sem dignissim, at fringilla diam sollicitudin.
+    Donec id risus aliquam, ultrices diam et, mattis ipsum.
+    Nulla venenatis lectus non velit molestie faucibus.
+    Proin fringilla ex in sapien tincidunt, a interdum elit tempor</p>
+    `
+
+    container.innerHTML= "";
+    container.appendChild(row)
+})
+
+
+//VARIABLES
+let contain = document.querySelector('.contenido');
+let question5 = document.getElementById('question5');
+
+
+question5.addEventListener('click',() =>{
+    const row = document.createElement('div')
+    row.innerHTML = `
+    <section> 
+    <h1 class="title">${question5.textContent}</h1>
+    <div class="divider"></div>
+    </section>
+    <p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Curabitur ac mi volutpat, feugiat est eget, bibendum nulla.
+    Curabitur ut velit id odio ullamcorper porttitor consectetur at sem.
+    Cras tempor odio ac eros suscipit scelerisque.
+    Aenean rutrum lacus ut sem dignissim, at fringilla diam sollicitudin.
+    Donec id risus aliquam, ultrices diam et, mattis ipsum.
+    Nulla venenatis lectus non velit molestie faucibus.
+    Proin fringilla ex in sapien tincidunt, a interdum elit tempor</p>
+    `
+
+    container.innerHTML= "";
+    container.appendChild(row)
+})
+
+
+//VARIABLES
+let containe = document.querySelector('.contenido');
+let question6 = document.getElementById('question6');
+
+
+question6.addEventListener('click',() =>{
+    const row = document.createElement('div')
+    row.innerHTML = `
+    <section> 
+    <h1 class="title">${question6.textContent}</h1>
+    <div class="divider"></div>
+    </section>
+    <p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Curabitur ac mi volutpat, feugiat est eget, bibendum nulla.
+    Curabitur ut velit id odio ullamcorper porttitor consectetur at sem.
+    Cras tempor odio ac eros suscipit scelerisque.
+    Aenean rutrum lacus ut sem dignissim, at fringilla diam sollicitudin.
+    Donec id risus aliquam, ultrices diam et, mattis ipsum.
+    Nulla venenatis lectus non velit molestie faucibus.
+    Proin fringilla ex in sapien tincidunt, a interdum elit tempor</p>
+    `
+
+    container.innerHTML= "";
+    container.appendChild(row)
+})
+
+
+//VARIABLES
+let containerr = document.querySelector('.contenido');
+let question7 = document.getElementById('question7');
+
+
+question7.addEventListener('click',() =>{
+    const row = document.createElement('div')
+    row.innerHTML = `
+    <section> 
+    <h1 class="title">${question7.textContent}</h1>
+    <div class="divider"></div>
+    </section>
+    <p class="parrafo"> Phasellus euismod nisl at metus finibus imperdiet.
+     Aliquam lectus sapien, faucibus et dolor eget, porta suscipit risus.
+      Mauris id leo mi. Aliquam elementum semper nulla. Pellentesque nec gravida tellus. 
+      In fringilla malesuada aliquet. Pellentesque eget nunc orci.
+       Etiam id erat ut nisi pretium sollicitudin.</p>
+    `
+
+    container.innerHTML= "";
+    container.appendChild(row)
+})
+
+
